b2d787c808487fc822203eebab87ac1a2aee07c2
John Buford <buford@samrg.org>
2016-12-05 01:55:05 -0800

    changes

--- run.sh
+++ run.sh
@@ -4,13 +4,13 @@ set -m
 mongodb_cmd="mongod --storageEngine $STORAGE_ENGINE"
 cmd="$mongodb_cmd --httpinterface --rest --master  --smallfiles"
 
-#if [ "$AUTH" == "yes" ]; then
-#    cmd="$cmd --auth"
-#fi
+if [ "$AUTH" == "yes" ]; then
+    cmd="$cmd --auth"
+fi
 
-#if [ "$JOURNALING" == "no" ]; then
-#    cmd="$cmd --nojournal"
-#fi
+if [ "$JOURNALING" == "no" ]; then
+    cmd="$cmd --nojournal"
+fi
 
 if [ "$OPLOG_SIZE" != "" ]; then
     cmd="$cmd --oplogSize $OPLOG_SIZE"
@@ -18,8 +18,8 @@ fi
 
 $cmd &
 
-#if [ ! -f /data/db/.mongodb_password_set ]; then
-#    /set_mongodb_password.sh
-#fi
+if [ ! -f /data/db/.mongodb_password_set ]; then
+    /set_mongodb_password.sh
+fi
 
 fg
